dac65c702df5c1b6ed10619c8a100d11bfc3be9d
Ioannis Boutsikas <mgioanni@outlook.com>
2017-01-13 12:58:25 +0200

    Fixed redirect issues with authguard

--- src/shared/Guards/isAuthenticatedGuard.ts
+++ src/shared/Guards/isAuthenticatedGuard.ts
@@ -11,14 +11,25 @@ export class IsAuthenticatedGuard implements CanActivate{
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>|Promise<boolean>|boolean {
-    // if(this.authService.isLoggedIn)
-    //   return true;
-    //
+    if(this.authService.isLoggedIn)
+      return Observable.of(true);
+
     // if(state.url === '/') {
     //   this.router.navigate(['/home']);
     //   return false;
     // }
-    //
+
+    if(this.authService.isAuthenticationPending) {
+      return this.authService.isAuthenticationPending$
+        .filter(value => value == false)
+        .switchMap(value => {
+          if(!this.authService.isLoggedIn)
+            this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+          return this.authService.isLoggedIn$;
+        });
+        // this.authService.isLoggedIn$);
+    }
+
     // if(this.authService.isAuthenticationPending) {
     //   this.authService.isLoggedIn$.skip(1).take(1).subscribe(value => {
     //     if(!value)
@@ -30,7 +41,7 @@ export class IsAuthenticatedGuard implements CanActivate{
     //   return false;
     // }
 
-    return true;
+    // return true;
   }
 
 }
--- src/shared/Services/authenticationService.ts
+++ src/shared/Services/authenticationService.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { UserActions } from '../Store/Actions/userActions';
 import { User } from '../Store/Models/user';
-import { BehaviorSubject, Subscription, Observable } from 'rxjs';
+import { BehaviorSubject, Subscription, Observable, Subject } from 'rxjs';
 import { IAppState } from '../Store/Reducers/index';
 import { CustomHttp } from './customHttp';
 import { BASE_URL } from '../Constants/settings';
@@ -11,38 +11,51 @@ import { BASE_URL } from '../Constants/settings';
 export class AuthenticationService {
 
   private meSub: Subscription;
+
   private loggedInSubsject: BehaviorSubject<boolean>;
   get isLoggedIn(): boolean {
     return this.loggedInSubsject.getValue();
   }
-
-  get isAuthenticationPending(): boolean {
-    return this._isAuthenticationPending;
-  }
-
-  private _isAuthenticationPending: boolean = false;
-
-
   get isLoggedIn$() : Observable<boolean> {
     return this.loggedInSubsject.asObservable();
   }
 
+
+  private authenticationPendingSubject: Subject<boolean>;
+  private _isAuthenticationPending: boolean = false;
+  get isAuthenticationPending(): boolean {
+    return this._isAuthenticationPending;
+  }
+
+  get isAuthenticationPending$(): Observable<boolean> {
+    return this.authenticationPendingSubject.asObservable();
+  }
+
+
+
+
   constructor(private store: Store<IAppState>, private userActions: UserActions, private chttp: CustomHttp) {
     this.loggedInSubsject = new BehaviorSubject<boolean>(false);
+    this.authenticationPendingSubject = new Subject<boolean>();
+
+    this.authenticationPendingSubject.subscribe(value => {
+      this._isAuthenticationPending = value;
+    })
 
     this.store.select('user').subscribe((user: User) => {
-      this._isAuthenticationPending = false;
       if (user) {
         this.loggedInSubsject.next(true);
       } else {
         this.loggedInSubsject.next(false);
       }
+      this.authenticationPendingSubject.next(false);
     });
   }
 
   login(username: string, password: string, remember: boolean) {
     let rvalue = remember ? '1' : '0';
-    this._isAuthenticationPending = true;
+    this.authenticationPendingSubject.next(true);
+
     this.store.dispatch(this.userActions.userLogin(username, password, rvalue));
   }
 
@@ -51,7 +64,8 @@ export class AuthenticationService {
   }
 
   getMe() {
-    this._isAuthenticationPending = true;
+    this.authenticationPendingSubject.next(true);
+
     if (this.meSub) {
       this.meSub.unsubscribe();
     }
@@ -59,7 +73,11 @@ export class AuthenticationService {
       .map(res => res.json().user)
       .subscribe(
         user => this.store.dispatch(this.userActions.userLoginSuccess(user)),
-        err => console.log('assuming fresh login'),
+        err => {
+          console.log('assuming fresh login');
+          this.store.dispatch(this.userActions.userLoginFail());
+          this.authenticationPendingSubject.next(false);
+        },
         () => {}
       );
   }
