d42beed68f3bbcd23a38d06508c9a0b707e8f794
Ioannis Boutsikas <mgioanni@outlook.com>
2017-03-24 17:56:00 +0200

    Done with units

--- src/shared/Services/unit.service.ts
+++ src/shared/Services/unit.service.ts
@@ -12,15 +12,27 @@ export class UnitService {
   constructor(private chttp: CustomHttp, private store: Store<IAppState>) {
 
   }
-  // NOTE: Leave as is, used for auto-loading mainly
+
   public loadUnits(): void {
     this.store.dispatch(UnitActions.loadUnits());
   }
 
-  public getUnit(unit_id: number): Observable<Response> {
-    this.store.dispatch(UnitActions.loadUnit(unit_id));
+  public loadUnits$(): Observable<Response> {
+    return this.chttp.get(BASE_URL + '/units?includes=assignments&compact=true')
+      .map(res => res.json().units)
+      .do(units => this.store.dispatch(UnitActions.loadUnitsSuccess(units)))
+      .catch(err => {
+        this.store.dispatch(UnitActions.loadUnitsFail());
+        return Observable.throw(err);
+      })
+  }
 
-    return this.chttp.get(`${BASE_URL}/units/${unit_id}?include=assignments&compact=true`)
+  public getUnit(unit_id: number): void {
+    this.store.dispatch(UnitActions.loadUnit(unit_id));
+  }
+
+  public getUnit$(unit_id: number): Observable<Response> {
+    return this.chttp.get(`${BASE_URL}/units/${unit_id}?includes=assignments&compact=true`)
       .map(res => res.json().unit)
       .do(unit => this.store.dispatch(UnitActions.loadUnitSuccess(unit)))
       .catch(err => {
