29dcf37a63487fcc15b86c2791a6c9fdb579ea03
Ioannis Boutsikas <mgioanni@outlook.com>
2017-06-29 17:33:20 +0300

    Auto stash before merge of "develop" and "origin/develop"

--- src/shared/Constants/settings.ts
+++ src/shared/Constants/settings.ts
@@ -1,4 +1,4 @@
-  import { GameSettings } from '../Store/Models/game-settings';
+import { GameSettings } from '../Store/Models/game-settings';
 export const BASE_URL= "http://capstoneed-api.org:21992/v1";
 export const STUDENT_URL = "http://student.capstoneed-dev.org:8080";
 export const LECTURER_URL = "http://lecturer.capstoneed-dev.org:8085";
